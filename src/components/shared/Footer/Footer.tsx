11452f7768a937781e9397257d45714ab5b80c7d
emamjion <emamjion@gmail.com>
2025-01-15 11:59:42 +0600

    commit for testing

--- src/components/shared/Footer/Footer.tsx
+++ src/components/shared/Footer/Footer.tsx
@@ -3,14 +3,24 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <Box bgcolor='rgb(17,26,34)' py={5}>
-      <Stack direction='row' gap={4} color= >
-          <Typography component={Link} href='/consultation'>Consultation</Typography>
-          <Typography component={Link} href='/health-plans'>Health Plans</Typography>
-          <Typography component={Link} href='/medicine'>Medicine</Typography>
-          <Typography component={Link} href='/diagnostics'>Diagnostics</Typography>
-          <Typography component={Link} href='/ngo'>NGOs</Typography>
-        </Stack>
+    <Box bgcolor="rgb(17,26,34)" py={5}>
+      <Stack direction="row" justifyContent='center' gap={4}>
+        <Typography color="#ffff" component={Link} href="/consultation">
+          Consultation
+        </Typography>
+        <Typography color="#ffff" component={Link} href="/health-plans">
+          Health Plans
+        </Typography>
+        <Typography color="#ffff" component={Link} href="/medicine">
+          Medicine
+        </Typography>
+        <Typography color="#ffff" component={Link} href="/diagnostics">
+          Diagnostics
+        </Typography>
+        <Typography color="#ffff" component={Link} href="/ngo">
+          NGOs
+        </Typography>
+      </Stack>
     </Box>
   );
 };
